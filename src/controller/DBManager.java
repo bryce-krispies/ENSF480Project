068f713f27b22a8f0fbf558dcd76e7b5abe067d4
coolsa <me@cloudchagnon.xyz>
2020-11-27 15:40:53 -0700

    DBManager beginning implementation

---
+++ src/controller/DBManager.java
@@ -0,0 +1,70 @@
+package controller;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+import model.Movie;
+import model.RegisteredUser;
+import model.Seat;
+import model.Showtime;
+import model.Theatre;
+
+
+public class DBManager {
+	public static void main(String args[]) {
+		//DBManager debug here!
+		
+	}
+	DBManager(){
+		//if using sql, connect to db here.
+		//but we are just using serialized files.
+	}
+	ArrayList<RegisteredUser> importRU(){
+		//open RUs file, import all registered users into here.
+		ArrayList<RegisteredUser> result = (ArrayList<RegisteredUser>) readFile("RegisteredUsers.db") ;
+		return result;
+	}
+	ArrayList<Theatre> importTheatres(){
+		//open theatres file, import all into here.
+		ArrayList<Theatre> result = new ArrayList<Theatre>();
+		return result;
+	}
+	ArrayList<Movie> importMovies(){
+		ArrayList<Movie> result = new ArrayList<Movie>();
+		return result;
+	}
+	ArrayList<Showtime> importShowtimes(){
+		ArrayList<Showtime> result = new ArrayList<Showtime>();
+		return result;
+	}
+	ArrayList<Seat> importSeats(){
+		ArrayList<Seat> result = new ArrayList<Seat>();
+		return result;
+	}
+	
+	public Object readFile(String file) {
+		try {
+			ObjectInputStream oIn = new ObjectInputStream(new FileInputStream(file));
+			Object out = oIn.readObject();
+			oIn.close();
+			return out;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public void writeFile(String file, Object out) {
+		try {
+			ObjectOutputStream oOut = new ObjectOutputStream(new FileOutputStream(file));
+			oOut.writeObject(out);
+			oOut.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	//if needed, we can also store users and tickets in database, but need to check with yazan first.
+}
---
+++ src/model/RegisteredUser.java
@@ -0,0 +1,76 @@
+package model;
+
+public class RegisteredUser extends User{
+	private String name;
+	private String password;
+	private Credit credit;
+	private Cart cart;
+	private CreditCard creditCard;
+	private String address;
+
+	RegisteredUser(String name, String password, Credit credit, Cart cart, CreditCard creditCard, String address) {
+		this.setName(name);
+		this.setPassword(password);
+		this.setCredit(credit);
+		this.setCart(cart);
+		this.setCreditCard(creditCard);
+		this.setAddress(address);
+	}
+
+	RegisteredUser() {
+		this.setName(null);
+		this.setPassword(null);
+		this.setCredit(null);
+		this.setCart(null);
+		this.setCreditCard(null);
+		this.setAddress(null);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Credit getCredit() {
+		return credit;
+	}
+
+	public void setCredit(Credit credit) {
+		this.credit = credit;
+	}
+
+	public Cart getCart() {
+		return cart;
+	}
+
+	public void setCart(Cart cart) {
+		this.cart = cart;
+	}
+
+	public CreditCard getCreditCard() {
+		return creditCard;
+	}
+
+	public void setCreditCard(CreditCard creditCard) {
+		this.creditCard = creditCard;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
